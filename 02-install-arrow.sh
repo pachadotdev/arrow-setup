2c0332c3cafa7981eacb67f31fc330a7e3241870
Mauricio Vargas <mavargas11@uc.cl>
2021-05-23 17:51:13 -0400

    R_LD_LIBRARY_PATH

--- 02-install-arrow.sh
+++ 02-install-arrow.sh
@@ -10,6 +10,7 @@ if [[ $ARROW_HOME == "" ]]
 then
  echo 'adding ARROW_HOME to .bashrc'
  printf '\n#ARROW VARS\nexport ARROW_HOME=~/.arrow' | tee -a ~/.bashrc
+ echo '\n'
 else
  echo 'ARROW_HOME is already set'
 fi
@@ -17,15 +18,24 @@ fi
 if [[ $LD_LIBRARY_PATH == "" ]]
 then
  echo 'adding LD_LIBRARY_PATH to .bashrc'
- printf '\nexport LD_LIBRARY_PATH=${ARROW_HOME}/lib:${LD_LIBRARY_PATH}' | tee -a ~/.bashrc
+ printf '\nexport LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${ARROW_HOME}/lib' | tee -a ~/.bashrc
 else
  echo 'LD_LIBRARY_PATH is already set'
+fi
+
+if [[ $R_LD_LIBRARY_PATH == "" ]]
+then
+ echo 'adding R_LD_LIBRARY_PATH to .bashrc'
+ printf '\nexport R_LD_LIBRARY_PATH=${LD_LIBRARY_PATH}' | tee -a ~/.bashrc
+else
+ echo 'R_LD_LIBRARY_PATH is already set'
 fi
 
 if [[ $ARROW_R_DEV == "" ]]
 then
  echo 'adding ARROW_R_DEV to .bashrc'
  printf '\nexport ARROW_R_DEV=true' | tee -a ~/.bashrc
+ echo '\n'
 else
  echo 'ARROW_R_DEV is already set'
 fi
@@ -34,6 +44,7 @@ if [[ $PKG_CONFIG_PATH == "" ]]
 then
  echo 'adding PKG_CONFIG_PATH to .bashrc'
  printf '\nexport PKG_CONFIG_PATH=${ARROW_HOME}/lib/pkgconfig' | tee -a ~/.bashrc
+ echo '\n'
 else
  echo 'PKG_CONFIG_PATH is already set'
 fi
@@ -42,6 +53,7 @@ if [[ $ARROW_WITH_BZ2 == "" ]]
 then
  echo 'adding ARROW_WITH_BZ2 to .bashrc'
  printf '\nexport ARROW_WITH_BZ2=ON' | tee -a ~/.bashrc
+ echo '\n'
 else
  echo 'ARROW_WITH_BZ2 is already set'
 fi
@@ -50,6 +62,7 @@ if [[ $ARROW_WITH_ZLIB == "" ]]
 then
  echo 'adding ARROW_WITH_ZLIB to .bashrc'
  printf '\nexport ARROW_WITH_ZLIB=ON' | tee -a ~/.bashrc
+ echo '\n'
 else
  echo 'ARROW_WITH_ZLIB is already set'
 fi
@@ -58,6 +71,7 @@ if [[ $ARROW_WITH_ZSTD == "" ]]
 then
  echo 'adding ARROW_WITH_ZSTD to .bashrc'
  printf '\nexport ARROW_WITH_ZSTD=ON' | tee -a ~/.bashrc
+ echo '\n'
 else
  echo 'ARROW_WITH_ZSTD is already set'
 fi
@@ -66,6 +80,7 @@ if [[ $ARROW_WITH_LZ4 == "" ]]
 then
  echo 'adding ARROW_WITH_LZ4 to .bashrc'
  printf '\nexport ARROW_WITH_LZ4=ON' | tee -a ~/.bashrc
+ echo '\n'
 else
  echo 'ARROW_WITH_LZ4 is already set'
 fi
@@ -74,6 +89,7 @@ if [[ $ARROW_WITH_BROTLI == "" ]]
 then
  echo 'adding ARROW_WITH_BROTLI to .bashrc'
  printf '\nexport ARROW_WITH_BROTLI=ON' | tee -a ~/.bashrc
+ echo '\n'
 else
  echo 'ARROW_WITH_BROTLI is already set'
 fi
@@ -82,6 +98,7 @@ if [[ $ARROW_WITH_SNAPPY == "" ]]
 then
  echo 'adding ARROW_WITH_SNAPPY to .bashrc'
  printf '\nexport ARROW_WITH_SNAPPY=ON' | tee -a ~/.bashrc
+ echo '\n'
 else
  echo 'ARROW_WITH_SNAPPY is already set'
 fi
@@ -90,6 +107,7 @@ if [[ $ARROW_BUILD_TESTS == "" ]]
 then
  echo 'adding ARROW_BUILD_TESTS to .bashrc'
  printf '\nexport ARROW_BUILD_TESTS=OFF' | tee -a ~/.bashrc
+ echo '\n'
 else
  echo 'ARROW_BUILD_TESTS is already set'
 fi
@@ -98,14 +116,16 @@ if [[ $ARROW_PARQUET == "" ]]
 then
  echo 'adding ARROW_WITH_PARQUET to .bashrc'
  printf '\nexport ARROW_PARQUET=ON' | tee -a ~/.bashrc
+ echo '\n'
 else
  echo 'ARROW_WITH_PARQUET is already set'
 fi
 
-if [[ $ARROW_PYTHON == "" ]]
+if [[ $ARROW_WITH_PYTHON == "" ]]
 then
  echo 'adding ARROW_PYTHON to .bashrc'
  printf '\nexport ARROW_WITH_PYTHON=OFF' | tee -a ~/.bashrc
+ echo '\n'
 else
  echo 'ARROW_WITH_PYTHON is already set'
 fi
@@ -114,6 +134,7 @@ if [[ $ARROW_COMPUTE == "" ]]
 then
  echo 'adding ARROW_COMPUTE to .bashrc'
  printf '\nexport ARROW_COMPUTE=ON' | tee -a ~/.bashrc
+ echo '\n'
 else
  echo 'ARROW_COMPUTE is already set'
 fi
@@ -122,6 +143,7 @@ if [[ $ARROW_CSV == "" ]]
 then
  echo 'adding ARROW_CSV to .bashrc'
  printf '\nexport ARROW_CSV=ON' | tee -a ~/.bashrc
+ echo '\n'
 else
  echo 'ARROW_CSV is already set'
 fi
@@ -130,6 +152,7 @@ if [[ $ARROW_DATASET == "" ]]
 then
  echo 'adding ARROW_DATASET to .bashrc'
  printf '\nexport ARROW_DATASET=ON' | tee -a ~/.bashrc
+ echo '\n'
 else
  echo 'ARROW_DATASET is already set'
 fi
@@ -138,6 +161,7 @@ if [[ $ARROW_FILESYSTEM == "" ]]
 then
  echo 'adding ARROW_FILESYSTEM to .bashrc'
  printf '\nexport ARROW_FILESYSTEM=ON' | tee -a ~/.bashrc
+ echo '\n'
 else
  echo 'ARROW_FILESYSTEM is already set'
 fi
@@ -146,6 +170,7 @@ if [[ $ARROW_JEMALLOC == "" ]]
 then
  echo 'adding ARROW_JEMALLOC to .bashrc'
  printf '\nexport ARROW_JEMALLOC=ON' | tee -a ~/.bashrc
+ echo '\n'
 else
  echo 'ARROW_JEMALLOC is already set'
 fi
@@ -154,6 +179,7 @@ if [[ $ARROW_JSON == "" ]]
 then
  echo 'adding ARROW_JSON to .bashrc'
  printf '\nexport ARROW_JSON=ON' | tee -a ~/.bashrc
+ echo '\n'
 else
  echo 'ARROW_JSON is already set'
 fi
@@ -162,6 +188,7 @@ if [[ $CMAKE_BUILD_TYPE == "" ]]
 then
  echo 'adding CMAKE_BUILD_TYPE to .bashrc'
  printf '\nexport CMAKE_BUILD_TYPE=release' | tee -a ~/.bashrc
+ echo '\n'
 else
  echo 'CMAKE_BUILD_TYPE is already set'
 fi
@@ -170,6 +197,7 @@ if [[ $ARROW_INSTALL_NAME_RPATH == "" ]]
 then
  echo 'adding ARROW_INSTALL_NAME_RPATH to .bashrc'
  printf '\nexport ARROW_INSTALL_NAME_RPATH=OFF' | tee -a ~/.bashrc
+ echo '\n'
 else
  echo 'ARROW_INSTALL_NAME_RPATH is already set'
 fi
@@ -178,12 +206,14 @@ if [[ $ARROW_S3 == "" ]]
 then
  echo 'adding ARROW_S3 to .bashrc'
  printf '\nexport ARROW_S3=OFF' | tee -a ~/.bashrc
+ echo '\n'
 else
  echo 'ARROW_S3 is already set'
 fi
 
 # verify env vars
 source ~/.bashrc
+
 echo $ARROW_HOME
 echo $LD_LIBRARY_PATH
 echo $ARROW_R_DEV
